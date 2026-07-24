6ba2dff32d1f5b52871446ba52b057c7f720341a
simple83 <jinwoo882@gmail.com>
2026-07-24 18:35:34 +0900

    Mock Data

--- src/features/post/services/postApi.ts
+++ src/features/post/services/postApi.ts
@@ -1,4 +1,5 @@
-import type { PostEntity, CreatePostParams } from '../types/post.type';
+import { MOCK_POSTS } from '../mocks/mockPosts';
+import type { PostEntity, PostCategory, CreatePostParams } from '../types/post.type'; //
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
@@ -16,11 +17,12 @@ const getAuthHeader = (): { Authorization: string } => {
     return { Authorization: `bearer ${token}` };
 }
 
+/* 실제 postApi
 export const postApi = {
-/* 
+
     게시글 목록 조회 (카테고리 필터링 지원)
     GET /posts?tag=FREE 형식으로 호출
-*/
+
     getposts: async (category?: string): Promise<PostEntity[]> => {
         const queryParam = category ? `?tag=${category}` : '';
         
@@ -35,6 +37,24 @@ export const postApi = {
 
         return response.json();
     },
+*/
+
+//목업 데이터 테스트용 postApi
+export const postApi = {
+  // 🧪 백엔드 미구현 상태용 Mock 서비스 함수
+  getPosts: async (category?: PostCategory): Promise<PostEntity[]> => {
+    // 실제 네트워크 요청처럼 0.5초 딜레이 발생시킴 (로딩 상태 테스트용)
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // 카테고리 선택 시 해당하는 데이터만 필터링해서 리턴
+    if (category) {
+      return MOCK_POSTS.filter((post) => post.category === category);
+    }
+
+    // 전체 조회 시 데이터 전체 리턴
+    return MOCK_POSTS;
+  },
+
 
 /*
     게시글 작성
